2883cb335feb3cf8ee3645bb03eb6c1258de54c3
Valderli <derlly@ipam.org.br>
2018-05-09 17:28:10 -0300

    vários filtros de busca

--- crawler.py
+++ crawler.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from bs4 import BeautifulSoup
 import re
 import requests
@@ -24,25 +27,76 @@ def extract_links(content):
     
     return links
 
-parameters = [
+filters = [
     {'desmatamento', 'amazônia'},
     {'desmatamento', 'cerrado'},
     {'desmatamento', 'ipam'},
+    {'desmatamento', 'floresta'},
     {'floresta', 'amazônia'},
     {'floresta', 'cerrado'},
-    {'floresta', 'ipam'}
+    {'floresta', 'ipam'},
+    {'garimpo', 'amazônia'},
+    {'garimpo', 'cerrado'},
+    {'fogo', 'amazônia'},
+    {'fogo', 'cerrado'},
+    {'fogo', 'unidade de conservação'},
+    {'fogo', 'terra indígena'},
+    {'fogo', 'floresta'},
+    {'fogo', 'área protegida'},
+    {'invação', 'unidade de conservação'},
+    {'invação', 'terra indígena'},
+    {'perda', 'carbono', 'floresta'},
+    {'emissão', 'CO2', 'floresta'},
+    {'emissão', 'CO2', 'desmatamento'},
+    {'estoque', 'carbono', 'floresta'},
+    {'estoque', 'carbono', 'regeneração'},
+    {'map', 'biomas'},
+    {'agricultura', 'familiar', 'amazônia'},
+    {'agricultura', 'familiar', 'cerrado'},
+    {'agricultura', 'familiar', 'mato grosso'},
+    {'agricultura', 'familiar', 'acre'},
+    {'agricultura', 'familiar', 'rondônia'},
+    {'agricultura', 'familiar', 'amazonas'},
+    {'agricultura', 'familiar', 'para'},
+    {'agricultura', 'familiar', 'roraima'},
+    {'agricultura', 'familiar', 'amapá'},
+    {'agricultura', 'familiar', 'acre'},
+    {'extrativismo', 'amazônia'},
+    {'extrativismo', 'cerrado'},
+    {'extração', 'madeira', 'ilegal', 'amazônia'},
+    {'extração', 'madeira', 'ilegal', 'cerrado'},
+    {'conflitos', 'amazônia'},
+    {'conflitos', 'cerrado'},
+    {'pesquisa', 'ipam'},
+    {'pesquisadores', 'ipam'},
+    {'estudo', 'ipam'},
+    {'ipam', 'desenvolve', 'sistema'},
+    {'ipam', 'plataforma'},
+    {'ipam', 'alerta indígena'},
+    {'amazônia', 'seca'},
+    {'cerrado', 'seca'},
+    {'amazônia', 'seca', 'fogo'},
+    {'cerrado', 'seca', 'fogo'},
+    {'amazônia', 'soja', 'desmatamento'},
+    {'cerrado', 'soja', 'desmatamento'},
 ]
 
-payload = {'q': ['desmatamento', 'cerrado'], 'tbm':'nws', 'tbs': 'qdr:d'}
+seen_urls = set()
+for filter in filters:
+    
+    payload = {'q': filter, 'tbm':'nws', 'tbs': 'qdr:d'}
 
-search = requests.get("https://www.google.com.br/search", params=payload)
-links = extract_links(search.text)
+    search = requests.get("https://www.google.com.br/search", params=payload)
+    links = extract_links(search.text)
+    
+    print search.url
 
-for link in links:
-    page = requests.get(link)
-    title = extract_title(page.text)
-    #some_list = ['desmatamento', 'floresta', 'ameaças', 'amazônia', 'cerrado', 'fogo', 'deforestation', 'forest', 'fire', 'IPAM']
-    if title:
-        #if any(s in title.encode('utf-8') for s in some_list):
-            print(title)
-            print link
+    for link in links:
+        print link
+        if link not in seen_urls:
+            seen_urls.add(link)
+            page = requests.get(link)
+            title = extract_title(page.text)
+            if title:
+                print(title)
+                print link
